98ef6971d1a4719341553ec5ef466380c8c1bbf6
Iga <iga.naskret@gmail.com>
2019-04-11 11:08:45 +0200

    menu

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const location = document.querySelector(".location");
+let locations = document.querySelector(".location");
 const genre = document.querySelector(".genre");
 const date = document.querySelector(".date");
 const locationMenu = document.querySelector("#location-menu");
 const genreMenu = document.querySelector("#genre-menu");
 const dateMenu = document.querySelector("#date-menu");
 
-location.addEventListener("click", () =>  locationMenu.classList.toggle("hide"));
+locations.addEventListener("click", () => {
+    locationMenu.classList.toggle("hide");
+     if (!genreMenu.classList.contains("hide")) {
+        genreMenu.classList.add("hide");
+}});
+
+genre.addEventListener("click", () => {
+    genreMenu.classList.toggle("hide");
+    if (!locationMenu.classList.contains("hide")) {
+        locationMenu.classList.add("hide");
+    }
+});
