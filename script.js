0f1bd45f99b334a5d073028d824ee8141d9e1cf5
andrea-makarova <andreamakarova99@gmail.com>
2019-04-11 16:49:22 +0200

    desktop version done

--- script.js
+++ script.js
@@ -6,28 +6,31 @@ const genreMenu = document.querySelector("#genre-menu");
 const dateMenu = document.querySelector("#date-menu");
 
 locations.addEventListener("click", () => {
-            locationMenu.classList.toggle("hide");
-            if (!genreMenu.classList.contains("hide")) {
-                genreMenu.classList.add("hide");}
-                if (!dateMenu.classList.contains("hide")) {
-                    dateMenu.classList.add("hide");
-                }
+    locationMenu.classList.toggle("hide");
+    if (!genreMenu.classList.contains("hide")) {
+        genreMenu.classList.add("hide");
+    }
+    if (!dateMenu.classList.contains("hide")) {
+        dateMenu.classList.add("hide");
+    }
 });
 
-        genre.addEventListener("click", () => {
-                genreMenu.classList.toggle("hide");
-                if (!locationMenu.classList.contains("hide")) {
-                    locationMenu.classList.add("hide");}
-                    if (!dateMenu.classList.contains("hide")) {
-                        dateMenu.classList.add("hide");
-                    }
-                });
+genre.addEventListener("click", () => {
+    genreMenu.classList.toggle("hide");
+    if (!locationMenu.classList.contains("hide")) {
+        locationMenu.classList.add("hide");
+    }
+    if (!dateMenu.classList.contains("hide")) {
+        dateMenu.classList.add("hide");
+    }
+});
 
-            date.addEventListener("click", () => {
-                    dateMenu.classList.toggle("hide");
-                    if (!locationMenu.classList.contains("hide")) {
-                        locationMenu.classList.add("hide");
-                        if (!genreMenu.classList.contains("hide")) {
-                            genreMenu.classList.add("hide");
-                        }
-                    }});
+date.addEventListener("click", () => {
+    dateMenu.classList.toggle("hide");
+    if (!locationMenu.classList.contains("hide")) {
+        locationMenu.classList.add("hide");
+        if (!genreMenu.classList.contains("hide")) {
+            genreMenu.classList.add("hide");
+        }
+    }
+});
