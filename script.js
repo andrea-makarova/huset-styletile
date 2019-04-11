5bba920da99c7a6fbf2ee0e650a3ee9d57f5f0d3
Iga <iga.naskret@gmail.com>
2019-04-11 09:00:00 +0200

    menu location w/ script that has errors

---
+++ script.js
@@ -0,0 +1,4 @@
+let location = document.querySelector(".location");
+let locationMenu = document.querySelector("#location-menu");
+
+location.addEventListener("click", locationMenu.classList.toggle("hide"));
